13589e4d6d4abc3404ef830986bfa1991c628873
Kenneth James Uy <uy.kennethjames23@gmail.com>
2026-06-26 20:43:48 +0800

    Add LocationService for fetching user's current location upon request

---
+++ PropertyCaptureDemo/Services/LocationService.swift
@@ -0,0 +1,69 @@
+//
+//  LocationService.swift
+//  PropertyCaptureDemo
+//
+//  Created by Kenneth James Uy on 6/26/26.
+//
+
+import CoreLocation
+import Foundation
+
+@Observable
+final class LocationService: NSObject {
+
+  var currentLocation: CLLocation?
+
+  private let locationManager = CLLocationManager()
+
+  override init() {
+    super.init()
+
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+  }
+
+  func requestPermission() {
+    locationManager.requestWhenInUseAuthorization()
+  }
+
+  func startUpdatingLocation() {
+    locationManager.startUpdatingLocation()
+  }
+
+  func stopUpdatingLocation() {
+    locationManager.stopUpdatingLocation()
+  }
+}
+
+// MARK: CLLocationManagerDelegate
+
+extension LocationService: CLLocationManagerDelegate {
+
+  func locationManager(
+    _ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]
+  ) {
+    currentLocation = locations.last
+  }
+
+  func locationManager(
+    _ manager: CLLocationManager, didFailWithError error: any Error
+  ) {
+    print("Failed to get live location: \(error.localizedDescription)")
+  }
+
+//  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//    switch manager.authorizationStatus {
+//      case .authorizedAlways, .authorizedWhenInUse:
+//      requestLocation()
+//
+//    case .denied, .restricted:
+//      print("Location access denied")
+//
+//    case .notDetermined:
+//      break
+//
+//    default:
+//      break
+//    }
+//  }
+}
