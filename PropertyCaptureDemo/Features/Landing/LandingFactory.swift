90ae954956272e3e46839264ab16e1343334d303
Kenneth James Uy <uy.kennethjames23@gmail.com>
2026-06-26 17:21:13 +0800

    Add LandingFactory enum for creating LandingView

---
+++ PropertyCaptureDemo/Features/Landing/LandingFactory.swift
@@ -0,0 +1,15 @@
+//
+//  LandingFactory.swift
+//  PropertyCaptureDemo
+//
+//  Created by Kenneth James Uy on 6/26/26.
+//
+
+import SwiftUI
+
+enum LandingFactory {
+
+  static func makeLandingView() -> some View {
+    return LandingView()
+  }
+}
