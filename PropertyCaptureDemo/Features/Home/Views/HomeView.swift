6ac528d86d2b7f40388f07044a3c774aa2449801
Kenneth James Uy <uy.kennethjames23@gmail.com>
2026-06-26 18:36:21 +0800

    Add camera photo capture capability to HomeView

--- PropertyCaptureDemo/Features/Home/Views/HomeView.swift
+++ PropertyCaptureDemo/Features/Home/Views/HomeView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct HomeView: View {
 
+  @State private var capturedImage: UIImage? = nil
+
+  @State private var isImagePickerPresented: Bool = false
+
   var body: some View {
     Text("Home")
       .navigationTitle("Home")
       .navigationBarBackButtonHidden()
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          Button("", systemImage: "camera") {
+            isImagePickerPresented.toggle()
+          }
+        }
+      }
+      .fullScreenCover(isPresented: $isImagePickerPresented) {
+        ImagePickerView(image: $capturedImage)
+      }
   }
 }
 
