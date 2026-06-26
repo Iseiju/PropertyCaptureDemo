fc61c15e53644f1ed13365c17114be07fb6364b8
Kenneth James Uy <uy.kennethjames23@gmail.com>
2026-06-26 17:48:17 +0800

    Add router-based navigation from the landing view to the home view

--- PropertyCaptureDemo/Features/Landing/Views/LandingView.swift
+++ PropertyCaptureDemo/Features/Landing/Views/LandingView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct LandingView: View {
-  
+
+  @Environment(Router.self) private var router
+
   var body: some View {
     Text("Landing")
+      .task { await pushToHome() }
+  }
+}
+
+// MARK: Navigation
+
+extension LandingView {
+
+  private func pushToHome() async {
+    try? await Task.sleep(for: .seconds(1))
+
+    router.push(to: .home)
   }
 }
 
