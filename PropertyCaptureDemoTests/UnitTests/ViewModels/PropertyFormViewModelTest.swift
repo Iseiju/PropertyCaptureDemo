9d37c95f4eb0b76e5ab2483c974495e20bffd41e
Kenneth James Uy <uy.kennethjames23@gmail.com>
2026-06-27 13:37:07 +0800

    Add get reverse geocode API unit test cases

--- PropertyCaptureDemoTests/UnitTests/ViewModels/PropertyFormViewModelTest.swift
+++ PropertyCaptureDemoTests/UnitTests/ViewModels/PropertyFormViewModelTest.swift
@@ -11,6 +11,61 @@ import CoreLocation
 
 final class PropertyFormViewModelTest: XCTestCase {
 
+  @MainActor
+  func testGetReverseGeocodeInfoCalled() async {
+    let (sut, spy) = makeSpyPropertyFormVM()
+
+    try? await sut.getReverseGeocodeInfo()
+
+    XCTAssertEqual(spy.called, [.getReverseGeocodeInfo(7.087357181983118, 125.66729262828807)])
+  }
+
+  @MainActor
+  func testGetReverseGeocodeInfoSuccess() async {
+    let (sut, mock) = makeMockPropertyFormVM()
+    let stubAddress = Address(
+      road: "Circumferential Road",
+      quarter: nil,
+      suburb: "Limao",
+      city: "Samal",
+      region: "Davao Region",
+      postcode: "8119",
+      country: "Philippines"
+    )
+    let stubReverseGeocode = ReverseGeocodeResponse(
+      placeId: 261460442,
+      name: "Crusoe Cabins at Costa Azalea",
+      type: "beach_resort",
+      address: stubAddress
+    )
+
+    mock.getReverseGeocodeInfoResult = .success(stubReverseGeocode)
+
+    do {
+      try await sut.getReverseGeocodeInfo()
+
+      XCTAssertNotNil(sut.reverseGeocodeResponse)
+      XCTAssertEqual(sut.reverseGeocodeResponse?.name, "Crusoe Cabins at Costa Azalea")
+      XCTAssertEqual(sut.reverseGeocodeResponse?.address.road, "Circumferential Road")
+    } catch {
+      XCTFail("Get Reverse Geocode request should succeed")
+    }
+  }
+
+  @MainActor
+  func testGetReverseGeocodeInfoFailed() async {
+    let (sut, mock) = makeMockPropertyFormVM()
+    mock.getReverseGeocodeInfoResult = .failure(.badServerResponse(message: "Unknown Error"))
+
+    do {
+      try await sut.getReverseGeocodeInfo()
+
+      XCTFail("Get Reverse Geocode request should fail")
+    } catch {
+      XCTAssertNil(sut.reverseGeocodeResponse)
+      XCTAssertEqual(error.localizedDescription, "Unknown Error")
+    }
+  }
 }
 
 // MARK: Make SUT
@@ -20,7 +75,8 @@ extension PropertyFormViewModelTest {
   @MainActor
   private func makeSpyPropertyFormVM() -> (PropertyFormViewModel, GeocodingAPISpy) {
     let geocodingAPISpy = GeocodingAPISpy()
-    let sut = PropertyFormViewModel(Data(), CLLocation(), geocodingAPISpy)
+    let location = CLLocation(latitude: 7.087357181983118, longitude: 125.66729262828807)
+    let sut = PropertyFormViewModel(Data(), location, geocodingAPISpy)
 
     return (sut, geocodingAPISpy)
   }
@@ -28,9 +84,9 @@ extension PropertyFormViewModelTest {
   @MainActor
   private func makeMockPropertyFormVM() -> (PropertyFormViewModel, GeocodingAPIMock) {
     let geocodingAPIMock = GeocodingAPIMock()
-    let sut = PropertyFormViewModel(Data(), CLLocation(), geocodingAPIMock)
+    let location = CLLocation(latitude: 7.087357181983118, longitude: 125.66729262828807)
+    let sut = PropertyFormViewModel(Data(), location, geocodingAPIMock)
 
     return (sut, geocodingAPIMock)
   }
 }
-
